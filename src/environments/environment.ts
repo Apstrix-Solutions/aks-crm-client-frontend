57e9b457f6d9512910b5d400558c5641bc5deb46
sreelekshmi <sreelekshmiu96@gmail.com>
2022-11-21 14:44:26 +0530

    Enviorment updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,7 +14,7 @@ export const environment = {
   // version: env['npm_package_version'] + '-dev',
   version: '1.0.0',
   serverUrl: 'https://akstech.sg/auth/api/v1/',
-  apiUrl: 'http://localhost:3001/api/',
+  apiUrl: 'https://akstech.sg/auth/api/v1/',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'fr-FR'],
 };
