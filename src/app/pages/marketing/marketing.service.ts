d003c14221532c8145955fd68ebb2dc3b9b14b9d
sreelekshmi <sreelekshmiu96@gmail.com>
2023-01-05 23:09:05 +0530

    organic post craeated with fb and insta in image

--- src/app/pages/marketing/marketing.service.ts
+++ src/app/pages/marketing/marketing.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { environment } from '@env/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +10,49 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class MarketingService {
 
   constructor(private httpClient: HttpClient) { }
+  
+  public ownedPage() {
+    return this.httpClient.get(`${environment.smiApiUrl}${'owned-pages'}`,{observe: 'response'});
+  }
+  public clientPage() {
+    return this.httpClient.get(`${environment.smiApiUrl}${'client-pages'}`,{observe: 'response'});
+  }
+  public instaPage() {
+    return this.httpClient.get(`${environment.smiApiUrl}${'insta-adaccounts'}`,{observe: 'response'});
+  }
+  public addContent(data) {
+    console.log(data);
+    debugger;
+    return this.httpClient
+      .post(`${environment.smiApiUrl}${'activity'}`, JSON.stringify(data))
+      .pipe(retry(1), catchError(this.errorHandl));
+  }
+
+  public addedFiles(data:any){
+    let formParams = new FormData();
+    formParams.set('photo_attachment', data)
+    return this.httpClient.post(`${environment.smiApiUrl}${'upload-photo'}`,
+        formParams,
+        {observe: 'response'}
+    )
+    .pipe(retry(1),catchError(this.errorHandl));
+    //return this.httpClient.get(`${environment.smiApiUrl}${'upload-photo'}`,{observe: 'response'});
+  }
+  getPost(){
+    return this.httpClient.get(`${environment.smiApiUrl}${'posts'}`);
+  }
+    // Error handling
+    errorHandl(error) {
+      let errorMessage = '';
+      if (error.error instanceof ErrorEvent) {
+        // Get client-side error
+        errorMessage = error.error.message;
+      } else {
+        // Get server-side error
+        errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+      }
+      return throwError(() => {
+        return errorMessage;
+      });
+    }
 }
--- src/app/pages/marketing/organicpost/organicpost.component.ts
+++ src/app/pages/marketing/organicpost/organicpost.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,NgZone, OnInit } from '@angular/core';
 import { FacebookLoginProvider, SocialAuthService,  SocialUser, } from 'angularx-social-login';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MarketingService } from '../marketing.service';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-organicpost',
@@ -8,20 +12,37 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./organicpost.component.scss']
 })
 export class OrganicpostComponent implements OnInit {
-
+  newSmContentsForm!: FormGroup;
   loginForm!: FormGroup;
   socialUser!: SocialUser;
   isLoggedin?: boolean = undefined;
+  refreshToken: string;
+  facebookAuthToken: string;
+  listPage:  any = [];
+  listInstaPage: any =[];
+  activity_records: any=[];
+  files: File[] = [];
+  social_media_content: any=[];
+  uploadInfo:  any = [];
+  uploadedFileResponse:any =[];
+  smInfo: any = [];
+  smInfoResponse:any =[];
+  sDate:string;
+  ownershipInfo:string = '';
   constructor(
-    private formBuilder: FormBuilder,
-    private socialAuthService: SocialAuthService
+    private formBulider: FormBuilder,
+    private socialAuthService: SocialAuthService,
+    private marketingService: MarketingService,
+    public toastr: ToastrService,
+    private ngZone: NgZone,
+    private router: Router,
   ) {
     console.log(this.isLoggedin);
   }
 
 
   ngOnInit(): void {
-    this.loginForm = this.formBuilder.group({
+    this.loginForm = this.formBulider.group({
       email: ['', Validators.required],
       password: ['', Validators.required],
     });
@@ -29,12 +50,155 @@ export class OrganicpostComponent implements OnInit {
       this.socialUser = user;
       this.isLoggedin = user != null;
     });
+    this.socialAuthService.authState.subscribe((user) => {
+     // console.log(user);
+      this.facebookAuthToken = user.authToken;
+      localStorage.setItem('fbAccessToken', this.facebookAuthToken)
+      //console.log(this.facebookAuthToken);
+    })
+    this.newSmContentsForm = this.formBulider.group({
+      activity_title: [null,[Validators.required]],
+      ownership:[null,[Validators.required]],
+      activity_type:[1],
+      start_date:[null],
+      end_date:['0001-01-01 00:00:00'],
+      post_type:[null],
+      activity_records :this.formBulider.group([null]),
+      social_media_content:this.formBulider.group({
+        content_title:['Title of the Sample Post No:1 Created'],
+        content_description:[null],
+        content_file: []
+      }),
+      is_deleted: [false],
+      content_status: [1],
+      //page:[null],
+      //instaPage:[null]
+    });
+
   }
 
   loginWithFacebook(): void {
     this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID);
+    this.instaPage();
   }
   signOut(): void {
     this.socialAuthService.signOut();
+  }
+  onSelected(value:string): void {
+    this.ownershipInfo = value;
+    if (value == 'agency'){
+      this.ownedPage();
+      this.newSmContentsForm.controls['ownership'].setValue(true);
+    }
+    else if(value == 'client'){
+      this.clientPage();
+      this.newSmContentsForm.controls['ownership'].setValue(false);
+    }
+	}
+  ownedPage() {
+    this.marketingService.ownedPage().subscribe((data) => {
+    this.refreshToken = data.headers.get('refresh_token');
+    this.listPage = this.formatListPage( data['body']['data']['data']);
+    });
+  };
+  clientPage() {
+    this.marketingService.clientPage().subscribe((data) => {
+      this.refreshToken = data.headers.get('refresh_token');
+      this.listPage = this.formatListPage( data['body']['data']['data']);
+    });
+  };
+  instaPage() {
+    this.marketingService.instaPage().subscribe((data) => {
+      this.refreshToken = data.headers.get('refresh_token');
+      this.listInstaPage = this.formatListPage( data['body']['data']['data']);
+    });
+  };
+  scheduleDate(value:string):void{
+    this.sDate = value;
+    this.newSmContentsForm.controls['start_date'].setValue(value);
+  }
+  contentDescription(value:string):void{
+    this.newSmContentsForm.get(['social_media_content','content_description']).setValue(value);
+  }
+  onSelect(event) {
+    this.files.push(...event.addedFiles);
+    this.uploadFile();
+  }
+  onRemove(event) {
+    console.log(event);
+    this.files.splice(this.files.indexOf(event), 1);
+  }
+  formatListPage(data){
+  for(let i =0; i<data.length;i++){
+    data[i].index = i;
+  }
+  return data;
+  }
+  pageInfo(value):void{
+    let smData = [];
+    smData['sms_id'] = 1;
+    smData['page_id'] = this.listPage[value].id;
+    smData['page_name'] = this.listPage[value].global_brand_page_name;
+    smData['schedule_date'] = this.sDate;
+    this.smInfo.push(smData);
+  }
+  instaPageInfo(value):void{
+    let smData = [];
+    smData['sms_id'] = 2;
+    smData['page_id'] = this.listInstaPage[value].id;
+    smData['page_name'] = this.listInstaPage[value].username;
+    smData['schedule_date'] = this.sDate;
+     this.smInfo.push(smData);
+    // console.log(this.smInfo);
+  }
+  addContent() {
+   let validaction = this.setsmInfo();
+   if(validaction == true ){
+    this.marketingService.addContent(this.newSmContentsForm.value).subscribe((res) => {
+      if(res['status'] == 200) {
+        this.toastr.success(res['message'], 'Success!');
+      } else {
+        this.toastr.error(res['message'], 'Error!');
+      } 
+      this.ngZone.run(() => this.router.navigateByUrl('/list-post'));
+    });
+   }
+   else{
+    this.toastr.error('Error!- you must select aleast one soical media info');
+   }
+    
+  }
+  setsmInfo(){
+    debugger;
+    for(let i=0; i<this.smInfo.length; i++){
+      this.smInfo[i] ['schedule_date'] = this.sDate;
+    }
+    if(this.smInfo.length != 0){
+      this.newSmContentsForm.get(['activity_records']).setValue([this.smInfo]);
+      console.log([this.smInfo]);
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  uploadFile(){
+    
+    if(this.files){
+      let fileslength = this.files.length;
+      this.marketingService.addedFiles(this.files[fileslength-1]).subscribe( (res) => {
+        this.refreshToken = res.headers.get('refresh_token');
+        this.uploadedFileResponse ['id']=res['body']['data']['data'].id;
+        this.uploadedFileResponse ['file_url']=res['body']['data']['data'].url;
+        this.uploadInfo.push(this.uploadedFileResponse);
+        this.newSmContentsForm.get(['social_media_content','content_file']).setValue(this.uploadInfo);
+      })
+    }
+  }
+  onSubmit() {
+    this.addContent();
+  };
+  get f() {
+    return this.newSmContentsForm.controls;
   }
 }
