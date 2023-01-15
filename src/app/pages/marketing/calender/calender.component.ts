fe3ae98704e6a2c754b6f93e13769e4ab6964c42
sreelekshmi <sreelekshmiu96@gmail.com>
2023-01-15 23:24:06 +0530

    Integrated the calendar package in angular

--- src/app/pages/marketing/calender/calender.component.ts
+++ src/app/pages/marketing/calender/calender.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
+import dayGridPlugin from '@fullcalendar/daygrid';
 @Component({
   selector: 'app-calender',
   templateUrl: './calender.component.html',
   styleUrls: ['./calender.component.scss']
 })
 export class CalenderComponent implements OnInit {
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin]
+  };
 
   constructor() { }
 
